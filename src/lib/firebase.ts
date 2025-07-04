65d4ae2d3fe63eb054459a4545f140a905b842ac
Lintshiwe Pontsho <lintshiwe1452@gmail.com>
2025-07-04 00:44:33 +0000

    use this firebase config
    // Import the functions you need from the SDKs

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,10 +6,12 @@ import { getStorage } from "firebase/storage";
 const firebaseConfig = {
   apiKey: "AIzaSyA2aNmuGOStDibvAWMUDOceYbOnWMHosmE",
   authDomain: "coursecampus-b2c7a.firebaseapp.com",
+  databaseURL: "https://coursecampus-b2c7a-default-rtdb.firebaseio.com",
   projectId: "coursecampus-b2c7a",
-  storageBucket: "coursecampus-b2c7a.appspot.com",
+  storageBucket: "coursecampus-b2c7a.firebasestorage.app",
   messagingSenderId: "978236014348",
-  appId: "1:978236014348:web:80462151ff8b9dce671329"
+  appId: "1:978236014348:web:80462151ff8b9dce671329",
+  measurementId: "G-YDZVV5X299"
 };
 
 // Initialize Firebase
