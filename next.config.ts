a75b3bdc355f0fde67c9a6dfa5052ac4039a4050
Lintshiwe Pontsho <lintshiwe1452@gmail.com>
2025-07-04 20:40:00 +0000

    return the fucntion of displaying uploaded  files  on admin site return

--- next.config.ts
+++ next.config.ts
@@ -18,6 +18,10 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  experimental: {
+    // This is required for Next.js to work in a cross-origin environment like Firebase Studio.
+    allowedDevOrigins: ['https://*.cluster-3gc7bglotjgwuxlqpiut7yyqt4.cloudworkstations.dev'],
+  },
 };
 
 export default nextConfig;
